c403afb1d8c54c1914bf92e10cfbc73e61c40555
VimForLaNie <new.giant098@gmail.com>
2023-07-17 14:26:48 +0700

    added smui

--- dashboard/src/routes/+page.svelte
+++ dashboard/src/routes/+page.svelte
@@ -1,45 +1,19 @@
 <script lang="ts">
 	import '../app.css';
+	import Button, { Label } from '@smui/button';
+	import Textfield from '@smui/textfield';
+	import DataTable, { Head, Body, Row, Cell } from '@smui/data-table';
 
-	let table:HTMLTableElement;
+	const formFields = ["Cow's  Name", "Farm Name", "Cow's Genetic", "Father's Name", "Father's Genetic", "Mother's Name", "Mother's Genetic"]
+
+	let tempCow = ["", "", "", "", "", "", ""];
+	let birthdate = 0;
+	let weightAtBirth = 0;
+
+	let cowsData: any;
 	const showCows = () => {
-		fetch('http://localhost/api/cows')
-			.then(res => {console.log(res); return res.json();})
-			.then(data => {
-				table.innerHTML = `
-					<thead>
-						<tr>
-							<th>ID</th>
-							<th>Name</th>
-							<th>Farm ID</th>
-							<th>Genetic</th>
-							<th>Birthdate</th>
-							<th>Weight at Birth</th>
-							<th>Father Name</th>
-							<th>Father Genetic</th>
-							<th>Mother Name</th>
-							<th>Mother Genetic</th>
-						</tr>
-					</thead>
-					<tbody>
-						${data.map((cow: any) => `
-							<tr>
-								<td>${cow.id}</td>
-								<td>${cow.name}</td>
-								<td>${cow.farmId}</td>
-								<td>${cow.genetic}</td>
-								<td>${cow.birthdate}</td>
-								<td>${cow.weightAtBirth}</td>
-								<td>${cow.fatherName}</td>
-								<td>${cow.fatherGenetic}</td>
-								<td>${cow.motherName}</td>
-								<td>${cow.motherGenetic}</td>
-							</tr>
-						`).join('')}
-					</tbody>
-				`;
-			})
-			.catch(err => console.log(err));
+		cowsData = fetch('http://localhost/api/cows')
+			.then(async res => res.json());
 	}
 
 	const postData = (url: string, data: any) => {
@@ -53,30 +27,6 @@
 		.then(res => res.json())
 		.catch(err => console.log(err));
 	}
-	
-	let cowName: string = "Cow Name";
-	let cowId: string = "Cow ID";
-	let cowFarmId: string = "Farm ID";
-	let cowGenetic: string = "Genetic";
-	let cowBirthdate: Date = new Date();
-	let cowWeightAtBirth: number = 0;
-	let cowFatherName: string = "Father Name";
-	let cowFatherGenetic: string = "Father Genetic";
-	let cowMotherName: string = "Mother Name";
-	let cowMotherGenetic: string = "Mother Genetic";
-
-	$:creatingCow = {
-		id: cowId,
-		cowName: cowName,
-		farmId: cowFarmId,
-		genetic: cowGenetic,
-		birthdate: cowBirthdate,
-		weightAtBirth: cowWeightAtBirth,
-		fatherName: cowFatherName,
-		fatherGenetic: cowFatherGenetic,
-		motherName: cowMotherName,
-		motherGenetic: cowMotherGenetic
-	}
 </script>
 
 <svelte:head>
@@ -88,27 +38,53 @@
 <!-- <p class="text-xl m-auto">Welcome to the dashboard!</p> -->
 
 <div class="flex flex-row">
-	<div class="flex flex-col p-3 border-zinc-800 border-2 mx-2">
-		<input type="text" name="id" placeholder="Cow ID" required class="textInput" bind:value={cowId}/>
-		<input type="text" name="name" placeholder="Cow Name" required class="textInput"
-		bind:value={cowName}/>
-		<input type="text" name="farmId" placeholder="farmId" required class="textInput"
-		bind:value={cowFarmId}/>
-		<input type="text" name="genetic" placeholder="genetic" required class="textInput"
-		bind:value={cowGenetic}/>
-		<input type="date" name="birthdate" placeholder="birthdate" required class="textInput"
-		bind:value={cowBirthdate}/>
-		<input type="number" name="weightAtBirth" min=0 step=0.001 placeholder="weightAtBirth" required class="textInput"
-		bind:value={cowWeightAtBirth}/>
-		<input type="text" name="fatherName" placeholder="fatherName" required class="textInput" bind:value={cowFatherName}/>
-		<input type="text" name="fatherGenetic" placeholder="fatherGenetic" required class="textInput" bind:value={cowFatherGenetic}/>
-		<input type="text" name="motherName" placeholder="motherName" required class="textInput" bind:value={cowMotherName}/>
-		<input type="text" name="motherGenetic" placeholder="motherGenetic" required class="textInput" bind:value={cowMotherGenetic}/>
-		<input type="button" value="Insert Cow" class="p-4 border-2 bg-neutral-300 hover:bg-neutral-200 rounded-md font-bold cursor-pointer" on:click={() => {postData(`http://localhost/api/cow`, creatingCow); console.log(creatingCow)}}/>
+	<div class="flex flex-col p-3 border-zinc-800 border-2 mx-2 rounded-md">
+		{#each formFields as field, index}
+			<Textfield label={field} bind:value={tempCow[index]} class="mt-2" input$maxlength={30} />	
+			{/each}
+		<Textfield label="Birth Date" type="datetime-local" bind:value={birthdate} class="mt-2"/>
+		<Textfield label="Weight At Birth" type="number" bind:value={weightAtBirth} class="mt-2"/>
+		
+		<Button on:click={() => console.log("Yay")} class="mt-5" variant="raised">
+			<Label>Insert Cow</Label>
+		</Button>
 	</div>
 </div>
 <div class="w-full m-2">
-	<input value="Show All Cows" type="button" class="p-4 border-2 bg-neutral-300 hover:bg-neutral-200 rounded-md font-bold cursor-pointer" on:click={showCows}>
-	<table bind:this={table}>
-	</table>
+	<Button on:click={showCows} class="mt-5" variant="raised">
+		<Label>Show Cows</Label>
+	</Button>
+	{#if cowsData}
+	<DataTable stickyHeader table$aria-label="User list" style="width: 100%;">
+		<Head>
+		  <Row>
+			<Cell numeric>ID</Cell>
+			<Cell>Genetic</Cell>
+			<Cell>Birth Date</Cell>
+			<Cell>Weight At Birth</Cell>
+			<Cell>Father's Name</Cell>
+			<Cell>Father's Genetic</Cell>
+			<Cell>Mother's Name</Cell>
+			<Cell>Mother's Genetic</Cell>
+		  </Row>
+		</Head>
+		<Body>
+		  {#each cowsData as data, index}
+			<Row>
+			  <Cell numeric>{index}</Cell>
+				<Cell>{data.id}</Cell>
+				<Cell>{data.genetic}</Cell>
+				<Cell>{new Date(data.birthdate)}</Cell>
+				<Cell>{data.weightAtBirth}</Cell>
+				<Cell>{data.fatherName}</Cell>
+				<Cell>{data.fatherGenetic}</Cell>
+				<Cell>{data.motherName}</Cell>
+				<Cell>{data.motherGenetic}</Cell>
+			</Row>
+		  {/each}
+		</Body>
+	</DataTable>
+	{:else}
+		<p>No cows to show</p>
+	{/if}
 </div>
